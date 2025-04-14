70ae4b347d3f3556961fe019e2aeb5ea65cca155
John Doe <harlamov-di@yandex.ru>
2025-04-14 23:01:53 +0300

    Install Ocstore

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://lknl-dev.ru/admin/');
+define('HTTP_CATALOG', 'https://lknl-dev.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://lknl-dev.ru/admin/');
+define('HTTPS_CATALOG', 'https://lknl-dev.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/c/cioloo8s/lknl-dev.ru/public_html/admin/');
+define('DIR_SYSTEM', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/');
+define('DIR_IMAGE', '/home/c/cioloo8s/lknl-dev.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/c/cioloo8s/lknl-dev.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/c/cioloo8s/lknl-dev.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/c/cioloo8s/lknl-dev.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'cioloo8s_lknl');
+define('DB_PASSWORD', 'T!sgx16bcv05');
+define('DB_DATABASE', 'cioloo8s_lknl');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://lknl-dev.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://lknl-dev.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/c/cioloo8s/lknl-dev.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/');
+define('DIR_IMAGE', '/home/c/cioloo8s/lknl-dev.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/c/cioloo8s/lknl-dev.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/c/cioloo8s/lknl-dev.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/c/cioloo8s/lknl-dev.ru/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'cioloo8s_lknl');
+define('DB_PASSWORD', 'T!sgx16bcv05');
+define('DB_DATABASE', 'cioloo8s_lknl');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
